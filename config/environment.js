35ef663660b37a2f5fb8cfb85407651f22b4ae6c
Rafa <rafaelruizbernal@gmail.com>
2019-01-10 16:53:17 +0000

    Updated seeds file

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 
 
 const port = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/database-name'
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/database-name'
 module.exports = {
   dbUri: dbUri,
   port: port
